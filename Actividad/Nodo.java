3d8876d4d55099265849fd1721e5de30833b5623
Rosario Cahuana Pari <rosariocahuanapari@gmail.com>
2025-04-28 05:58:38 -0500

    Clase Nodo

---
+++ Actividad/Nodo.java
@@ -0,0 +1,11 @@
+public class Nodo<T> {
+    T data; // Dato almacenado en el nodo
+    Nodo<T> next; // Referencia al siguiente nodo en la lista
+
+
+    // Constructor
+    public Nodo(T data) {
+        this.data = data; // Asignamos el dato al nodo
+        this.next = null; // Inicialmente no apunta a ningún nodo (es el último)
+    }
+}
